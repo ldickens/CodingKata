f0a307b49fae352aa15d1d0a368b0e8d94231796
Leon Dickens <leon.dickens@hotmail.com>
2024-06-28 09:31:48 +0100

    Add project files.

---
+++ Anagram/Program.cs
@@ -0,0 +1,37 @@
+﻿const string word = "documenting";
+const string fileName = "words.txt";
+
+string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
+
+try
+{
+	StreamReader stream = new(path);
+	string line = stream.ReadLine();
+	
+	if (line != null)
+	{
+		string[] lineWords = line.Split(' ');
+
+		foreach (string target in lineWords)
+		{
+            if (IsAnagram(target))
+            {
+                
+            }
+        }
+	}
+
+	
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("File not found");
+}
+
+
+static bool IsAnagram(string target)
+{
+	char[] chars = target.ToCharArray();
+
+
+}
